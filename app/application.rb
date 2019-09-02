4ebb65651d3d7b3d670894659207cb0b13190111
Huy Do <huyddo40@gmail.com>
2019-09-02 21:40:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,14 +1,17 @@
 class Application
-def call(env)
-  resp = Rack::Response.new
-  req = Rack::Request.new(env)
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
   if req.path.match(/items/)
     item_name = req.path.split("/items/").last
     #turn /items/name into name
     item = @@items.find{|i| i.name == item_name}
       resp.write item.price
-    end
+  else 
+    resp.write "Rout not found"
+    resp.status = 404
+  end
 
   resp.finish
 end
