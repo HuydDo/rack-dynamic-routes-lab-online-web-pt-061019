246cbd48f48c32b5fe18134d6798a0ef0d7b5607
Huy Do <huyddo40@gmail.com>
2019-09-02 21:47:24 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       #turn /items/name into name
-      item = self.find{|i| i.name == item_name}
+      item = items.find{|i| i.name == item_name}
 
         # resp.write "item.price"
       item.price
