461a299fe023cb8ecc4594ccd3c788d371057b21
Huy Do <huyddo40@gmail.com>
2019-09-02 21:40:41 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,15 +3,16 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-  if req.path.match(/items/)
-    item_name = req.path.split("/items/").last
-    #turn /items/name into name
-    item = @@items.find{|i| i.name == item_name}
-      resp.write item.price
-  else
-    resp.write "Rout not found"
-    resp.status = 404
-  end
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      #turn /items/name into name
+      item = @@items.find{|i| i.name == item_name}
+        resp.write item.price
+    else
+      resp.write "Rout not found"
+      resp.status = 404
+    end
 
-  resp.finish
+    resp.finish
+  end
 end
